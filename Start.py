5bf9c7190f989c367e935d056ca987d643a5f49b
davianrosario13-boop <davianrosario13@gmail.com>
2026-07-04 14:16:44 +0100

    Completed first part of the Random Guesser, need to add defensive design to the program before adding more features.

--- Start.py
+++ Start.py
@@ -1,19 +1,28 @@
-import random
-import time
-print("Hello World!")
-time.sleep(2)
-print("Let's play a  game.")
-time.sleep(2.75)
-print("How about...")
-time.sleep(3)
-print("A guessing game!")
-time.sleep(1)
-print("Ye that sounds fun!")
-time.sleep(3)
-value = random.randint(1, 100)
-Guess = 0
-print(f"Okay I'm thinking of a number between 1 and 100. Can you guess what it is?")
-time.sleep(4)
-print("Don't worry I'll give you some clues.")
+import random #allows the use of picking a random number in the program
+import time #allows the use of a timer in the program
+messages = [
+    ("Hello World!",2),
+    ("Let's play a game.",2.75),
+    ("How about...",3),
+    ("A guessing game!",1),
+    ("Ye that sounds fun!",3),
+    ("Okay I'm thinking of a number between 1 and 100. Can you guess what it is?",4),
+    ("Don't worry I'll give you some clues, along the way.",3)
+]
+for i in range(0,len(messages),1):
+    msg, delay = messages[i]
+    print(msg)
+    time.sleep(delay)
+    if i == 5:
+        value = random.randint(1, 100)
+        Guess = 0
+Guess = int(input("Go! "))
 while value != Guess:
-    Guess = int(input("Go!"))
+    if Guess < 1 or Guess > 100:
+        print("I just said a number between 1 and 100.")
+    elif Guess < value:
+        print("My number is higher than your guess.")
+    else:
+        print("My number is lower than your guess.")
+    Guess = int(input("Try again! "))
+print("You guessed it! My number was ", str(value) + ".")
